8b18e9473eab1bdcff2c1b20d774b303953ac53f
azxca1731 <azxca1731@gmail.com>
2018-12-04 19:37:37 +0900

    익스프레스 완료

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 import request from "request";
 import cheerio from "cheerio";
-let url = "https://torrenthaja.com/bbs/board.php?bo_table=torrent_drama&page=1";
-console.log("Drama Crawler is running!");
-request(url, function(error, response, body) {
-  const $ = cheerio.load(body);
-  let dramaArray = [];
-  $(".td-subject").map((_, item) => {
-    dramaArray.push({
-      href: item.children[1].attribs.href,
-      title: $(item)
-        .children()
-        .text()
-    });
-  });
-  let toBeSavedArray = [];
-  dramaArray.map(item => {
-    request(item.href, (err, res, body) => {
-      const $ = cheerio.load(body);
-      console.log({
-        ...item,
-        magnet: $(".glyphicon-magnet")
-          .parent()
-          .attr("onclick")
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello It is Crawler");
+});
+
+app.get("/api", async (req, res) => {
+  let url =
+    "https://torrenthaja.com/bbs/board.php?bo_table=torrent_drama&page=1";
+
+  request(url, function(error, response, body) {
+    const $ = cheerio.load(body);
+    let dramaArray = [];
+    $(".td-subject").map((_, item) => {
+      dramaArray.push({
+        href: item.children[1].attribs.href,
+        title: $(item)
+          .children()
+          .text()
       });
     });
+    let toBeSavedArray = [];
+    dramaArray.map(item => {
+      request(item.href, (err, res, body) => {
+        const $ = cheerio.load(body);
+        toBeSavedArray.push({
+          ...item,
+          magnet: $(".glyphicon-magnet")
+            .parent()
+            .attr("onclick")
+        });
+      });
+    });
+    res.json(toBeSavedArray);
   });
+});
+
+app.listen(80, () => {
+  console.log("Drama Crawler is running!");
 });
