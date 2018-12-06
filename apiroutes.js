a54c0551a87d834793138980963dd80e82b9cdce
azxca1731 <azxca1731@gmail.com>
2018-12-06 23:49:52 +0900

    update only new video

--- apiroutes.js
+++ apiroutes.js
@@ -15,6 +15,7 @@ const dramaParser = dramaString => {
   const quality = others.substr(0, others.indexOf("p"));
   return { title, episode, broadcastdate, quality };
 };
+
 router.get("/example", async (req, res) => {
   console.log(dramaParser("비밀과 거짓말.E98.181206.720p-NEXT"));
 });
@@ -28,39 +29,47 @@ router.get("/", async (req, res) => {
     }
   };
   const dramaArray = [];
+  let recentVideoInfo = await VideoInfo.find()
+    .sort({ index: -1 })
+    .limit(1);
+  if (recentVideoInfo.length === 0) recentVideoInfo = [{ index: 0 }];
+  console.log(recentVideoInfo[0].index);
   let body = await request(options);
   const $ = cheerio.load(body);
 
   $(".td-subject").map((_, item) => {
+    let number = $(item)
+      .parent()
+      .prev()
+      .text()
+      .substr(1);
+    number =
+      number.substr(0, number.indexOf(",")) +
+      number.substr(number.indexOf(",") + 1, number.length);
     dramaArray.push({
-      index: $(item)
-        .parent()
-        .prev()
-        .text(),
+      index: number,
       href: item.children[1].attribs.href,
       crawledtitle: $(item)
         .children()
         .text()
     });
   });
-  const promiseArray = dramaArray.map(async item => {
-    body = await request(item.href);
-    const $ = cheerio.load(body);
-    return {
-      ...item,
-      magnet: $(".glyphicon-magnet")
-        .parent()
-        .attr("onclick")
-    };
-  });
+  const promiseArray = dramaArray
+    .filter(item => item.index > recentVideoInfo[0].index)
+    .map(async item => {
+      body = await request(item.href);
+      const $ = cheerio.load(body);
+      return {
+        ...item,
+        magnet: $(".glyphicon-magnet")
+          .parent()
+          .attr("onclick")
+      };
+    });
   let json = await Promise.all(promiseArray);
   json = json.map(async ({ index, crawledtitle, magnet }) => {
     const item = new VideoInfo();
-    let number = index.substr(1, index.length);
-    number =
-      number.substr(0, number.indexOf(",")) +
-      number.substr(number.indexOf(",") + 1, number.length);
-    item.index = number;
+    item.index = index;
     const origTitle = crawledtitle.substr(2, crawledtitle.length);
     const { title, episode, broadcastdate, quality } = dramaParser(origTitle);
     item.title = title;
